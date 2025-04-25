aae0e7ec37a369398bb1aa098b19ad681b5d0828
keba_e <fsdgfsdg@dfsdf.com>
2025-04-25 11:42:37 +0300

    3

--- tic_tac.py
+++ tic_tac.py
@@ -19,9 +19,48 @@ class TicTacToe:
                                command=lambda i=i: self.player_move(i))
             button.grid(row=i // 3, column=i % 3)
             self.buttons.append(button)
+
     def player_move(self, index):
         if self.board[index] == "":
             self.board[index] = self.player
+            self.buttons[index].config(text=self.player)
+            if self.check_game(self.player):
+                messagebox.showinfo("Гра закінчена", "Ти переміг!")
+            elif "" not in self.board:
+                messagebox.showinfo("Гра закінчена", "Нічия!!")
+                self.reset_game()
+            else:
+                self.root.after(500, self.comp_move)
+
+    def comp_move(self):
+        empty_indexes = [i for i, val in enumerate(self.board) if val == ""]
+        if not empty_indexes:
+            return
+        move = random.choice(empty_indexes)
+        self.board[move] = self.comp
+        self.buttons[move].config(text=self.comp)
+        if self.check_game(self.comp):
+            messagebox.showinfo("Гра закінчена", "Комп переміг!")
+        elif "" not in self.board:
+            messagebox.showinfo("Гра закінчена", "Нічия!!")
+            self.reset_game()
+
+    def check_game(self, symbol):
+        combos = [
+            (0, 1, 2), (3, 4, 5), (6, 7, 8),
+            (0, 3, 6), (1, 4, 7), (2, 5, 8),
+            (0, 5, 8), (2, 5, 6)
+        ]
+        for a,b,c in combos:
+            if self.board[a] == self.board[b] == self.board[c] == symbol:
+                return True
+        return False
+
+    def reset_game(self):
+        self.board = [""] * 9
+        for btn in self.buttons:
+            btn.config(text="")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
